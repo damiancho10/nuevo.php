2c092159f459fd8452807326c8da4695ec59b9ef
damiancho10 <127424235+damiancho10@users.noreply.github.com>
2023-07-31 10:27:25 -0500

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,19 @@
+    <?php
+
+    //variables de conexion
+    $serverName='localhost';
+    $database='futbol';
+    $userName='root';
+    $password='';
+
+    //crearm la conexion
+    $conn= mysqli_connect($serverName,$userName, $password, $database);
+
+    //check a la conexion
+
+    if(!$conn ){
+        die("Error de Conexion" .sqli_connect_error());
+    }
+    echo"Conexion Exitosa";
+    mysqli_close($conn);
+    ?>
